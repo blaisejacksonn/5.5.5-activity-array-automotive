af897e7263cc111af5e706e6af21a1f22593a2f4
Blaise Jackson <blaise.jackson12@gmail.com>
2022-10-16 15:50:36 -0400

    addding properties

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,23 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass")
 
+class car extends VehicleModule {
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5
+        this.passenger = 0
+        this.numberOfWheels = 4
+        this.maximumSpeed = 160
+        this.fuel = 10
+        this.scheduleService = false
+    }
+}
+
+
+
+
+
+
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
